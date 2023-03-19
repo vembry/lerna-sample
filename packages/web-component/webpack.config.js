e23bcc1009ed1c89a77241f6b451ef612dc607d3
vembry <airstrip99@gmail.com>
2023-03-19 22:03:40 +0700

    chore: redo babel

--- packages/web-component/webpack.config.js
+++ packages/web-component/webpack.config.js
@@ -1,18 +1,15 @@
-module: {
+module.exports = {
+  // ...
+  module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react'
-            ]
-          }
-        }
-      }
-    ]
-  }
-  
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  // ...
+};
