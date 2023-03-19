c372117502309a2fe02db660022add1bdcb46c2a
vembry <airstrip99@gmail.com>
2023-03-19 22:00:18 +0700

    chore: place babel + webpack config

---
+++ packages/web-component/webpack.config.js
@@ -0,0 +1,18 @@
+module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  }
+  
