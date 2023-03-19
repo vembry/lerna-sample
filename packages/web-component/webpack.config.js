98e6dad641770d0e0b34b21f1e34bd441900adee
vembry <airstrip99@gmail.com>
2023-03-20 00:57:54 +0700

    fix: babel again

--- packages/web-component/webpack.config.js
+++ packages/web-component/webpack.config.js
@@ -2,19 +2,19 @@
 module.exports = {
   // ...
   module: {
+    // exclude node_modules
     rules: [
-      // ...
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
   },
   // ...
 };
