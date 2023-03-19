3262ab364e93de11ce474ba13532bb8de44e7ad4
vembry <airstrip99@gmail.com>
2023-03-19 22:46:02 +0700

    fix: comma?

--- packages/web-a/webpack.config.js
+++ packages/web-a/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-react", "@babel/preset-env"],
-          },
+          }
         },
       },
     ],
