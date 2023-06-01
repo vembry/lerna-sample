8dc9f86ef0ce43c1c6e23a83b8ee0c74b5b253e8
vembry <airstrip99@gmail.com>
2023-06-02 02:23:30 +0700

    chore: setup api handler

---
+++ packages/api/src/main.ts
@@ -0,0 +1,12 @@
+import * as http from "http";
+
+const server = http.createServer((req, res) => {
+  res.write("Hello World!");
+  res.end();
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000. Go to http://localhost:3000/");
+});
+
+server.close();
